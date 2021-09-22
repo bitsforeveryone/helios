8467d7cae7e5fc33d1788357be35e9306069a37f
John Rhyme <cockatiel424@hotmail.com>
2021-09-22 16:14:36 -0400

    auth v7

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ secrets=open("secrets/misc")
 SECRETS=json.load(secrets)
 secrets.close()
 
-DISCORD_ENDPOINT = "https://discord.com/api/oauth2/authorize?client_id=889907808852656178&redirect_uri=https%3A%2F%2Fhelios.c3t.eecs.net%2Fauth&response_type=token&scope=identify%20guilds"
+DISCORD_ENDPOINT = "https://discord.com/api/oauth2/authorize?client_id=889907808852656178&redirect_uri=https%3A%2F%2Fhelios.c3t.eecs.net%2Fauth?&response_type=token&scope=identify%20guilds"
 DISCORD_REQUESTS=[]
 DISCORD_API="https://discordapp.com/api"
 
