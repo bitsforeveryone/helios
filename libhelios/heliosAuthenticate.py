9e6e996de7994428f5f9731f04f06e023e953529
John Rhyme <cockatiel424@hotmail.com>
2021-09-22 16:13:01 -0400

    auth v6

--- libhelios/heliosAuthenticate.py
+++ libhelios/heliosAuthenticate.py
@@ -5,8 +5,6 @@ import requests
 GET_USER="/users/@me"
 GET_USER_GUILDS="/users/@me/guilds"
 
-DISCORD_ENDPOINT = "https://discord.com/api/oauth2/authorize?client_id=889907808852656178&redirect_uri=https%3A%2F%2Fhelios.c3t.eecs.net%2Fauth&response_type=token&scope=identify%20guilds"
-DISCORD_REQUESTS=[]
 DISCORD_API="https://discordapp.com/api"
 
 # given a request object, return a token after verifying request is legitimate
